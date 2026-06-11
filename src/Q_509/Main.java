e9a33add70e7d675d8136dfe57948ecac5d3a79b
myeonghwi <automaster5013@gmail.com>
2026-06-11 13:10:02 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/Q_509/Main.java
@@ -0,0 +1,31 @@
+package Q_509;
+/*
+public class Main {
+    public static void main(String[] args) {
+        int num = -100;
+
+        System.out.println(num);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int positNum = 100;
+
+        System.out.println(-positNum);
+    }
+}
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int score = -100;
+
+        System.out.printf("%d\n", score);
+    }
+}
+
+
+
+
+
